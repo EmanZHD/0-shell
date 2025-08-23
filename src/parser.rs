bd1710ef494eafefe4e1aab897d0604b875a6718
hlamrani <hasnaelamrani3434@gmail.com>
2025-08-23 15:16:46 +0100

    Prompt with current directory

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,27 @@
-use std::io;
+use std::{io};
 use std::io::Write;
+use std::env;
+use colored::*;
+
+/*********🌟 Current Dir 🌟********/
+fn current() -> String {
+  let result: String = match env::current_dir() {
+    Ok(path) => {
+      match path.file_name() {
+        Some(file_name) => file_name.to_string_lossy().into_owned(),
+        None => String::from("/"),
+      }
+    },
+    Err(_e) => "/".to_string(),
+  };
+  result
+}
 
 /*********🌟 print_prompt 🌟********/
 pub fn print_prompt() {
-  let begin = "$";
-  
-  print!("{0} ", begin);
+  print!("{}", "~".bold().yellow());
+  print!("{}", current().bold().truecolor(199, 21, 133));
+  print!("{} ", "$".bold().yellow());
   io::stdout().flush().unwrap();
 }
 
