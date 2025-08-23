6df7d6d2510ad5c42a32da4d795c1b1ec47cc810
hlamrani <hasnaelamrani3434@gmail.com>
2025-08-23 17:34:17 +0100

    update main file

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,22 @@ use commands::cd::cd;
 fn main() {
     println!("{GREEN}{}{RESET}", TITLE);
 
+    loop {
+        print_prompt();
+        let (keyword, arguments) = read_input();
+        handle_cmds(keyword, arguments);
+    }
+}
+
+pub fn handle_cmds(keyword: String, arguments: Vec<String>) {
     let mut dispatcher: HashMap<&str, fn(Vec<String>)> = HashMap::new();
     dispatcher.insert("pwd", pwd);
     dispatcher.insert("exit", exit);
     dispatcher.insert("guide", guide);
-    dispatcher.insert("cd", cd);
-    // println!("MAP {:?}", dispatcher);
-    loop {
-        print_prompt();
-        let (keyword, arguments) = read_input();
-        match dispatcher.get(&keyword.as_str()) {
-            Some(func) => func(arguments),
-            None => println!("0-shell: Command Not Found: {} ☹️", keyword),
-        }
+    dispatcher.insert("cd", cd); 
+
+    match dispatcher.get(&keyword.as_str()) {
+        Some(func) => func(arguments),
+        None => println!("0-shell: Command Not Found: {} ☹️", keyword),
     }
 }
