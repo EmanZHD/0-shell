8635e647636154772cff1dc1d9af22d6ad789bab
Hasnae 🍒 <125884813+Hasnaaaae@users.noreply.github.com>
2025-08-24 21:16:30 +0000

    command history done

--- src/commands/history.rs
+++ src/commands/history.rs
@@ -1,12 +1,9 @@
 pub fn history(his: &mut Vec<(i32, String)>) {
-   if his.is_empty() {
-     println!("This is your history 🤗");
-     return;
-   }   // toutours il n'est jamais empty car il y a une commande qui est history
+   println!("This is your history 🤗");
    let lenght = his.len().to_string();
    for col in his {
       let espace = lenght.len() - col.0.to_string().len();
-      let result= format!("{} {}", espace, col.1);  // n'est pas termine
+      let result= format!("{}{} {}", " ".repeat(espace), col.0, col.1);
       println!("{}", result);
    } 
 }
