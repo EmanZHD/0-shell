06e8616bec87b137c1fc18290e76f2b770dca470
houda jeon <houdaafebts678@gmail.com>
2025-08-26 17:32:00 +0100

    cat command done yaay 🥳​🥳​🥳​

--- src/commands/cat.rs
+++ src/commands/cat.rs
@@ -1,1 +1,30 @@
-// start
+use std::fs;
+use std::io::{self, Read};
+
+// 🥳​ here check input if have argument ymchi --> cat_file, makanch ya3na dar gha cat aymci --> only_cat 🥳​
+pub fn cat(args: Vec<String>) {
+    if args.is_empty() {
+        only_cat();
+    } else {
+        for filename in &args {
+            if let Err(e) = cat_file(filename) {
+                eprintln!("cat: {}: {} ☹️", filename, e);
+            }
+        }
+    }
+}
+
+// 💁‍♀️​ handle only cat 💁‍♀️​
+fn only_cat() {
+    println!("Reading from stdin (Ctrl+D to end) ☺️​:");
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    print!("{}", buffer);
+}
+
+// 💁‍♀️​ handle cat + plusieurs arg(files) 💁‍♀️​
+fn cat_file(filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let contents = fs::read_to_string(filename)?;
+    println!("{}", contents);
+    Ok(())
+}
