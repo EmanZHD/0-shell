4c88e4f249a5316b841834f74e153d6b2e72b879
Hasnaaaae <hasnaelamrani3434@gmail.com>
2025-08-28 09:48:28 +0100

    fix cd -

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -4,34 +4,64 @@ use std::env::set_current_dir;
 use crate::Params;
 
 pub fn cd(parameters: &mut Params) {
-    if parameters.args.len() == 0 {
-        go_to_home()
-    } else {
-        if &parameters.args[0] == "~" || &parameters.args[0] == "--" {
-            go_to_home();
-            return;
-        }
-        if parameters.args.len() == 1 && &parameters.args[0] == "-" {
-            if let Some(prev_dir) = parameters.previous_path.take() {
-                let current_dir = env::current_dir().ok();
-                if let Err(e) = env::set_current_dir(&prev_dir) {
-                    eprintln!("0-shell: cd: {}: {}", prev_dir.display(), e);
-                    parameters.previous_path = current_dir.into();
-                } else {
-                    println!("{}", prev_dir.display());
-                    parameters.previous_path = current_dir;
-                    println!("🚨 => {:?}", parameters.previous_path);
-                }
-            } else {
-                parameters.previous_path = env::current_dir().ok();
-            }
-            return;
-        }
-            let path = Path::new(&parameters.args[0]);
-            if let Err(_e) = set_current_dir(&path) {
-                eprintln!("⛔ 0-shell: No such file or directory {:?}", path);
-            }
+    let current_dir = env::current_dir().ok();
+    if parameters.args.is_empty() {
+        go_to_home();
+        parameters.previous_path = current_dir;
+        return;
     }
+    
+    match parameters.args[0].as_str() {
+       "~" | "--" => {
+           go_to_home();
+           parameters.previous_path = current_dir;  
+       }
+       "-" => {
+           if let Some(precedent) = parameters.previous_path.take() {
+              if let Err(e) = env::set_current_dir(&precedent) {
+                 eprintln!("0-shell: cd: {}: {}", precedent.display(), e);
+                 parameters.previous_path = Some(precedent); // fhaltma w9a3 mochkil tanraj3o lpath ly kan 9bel
+              }else {
+                 println!("{}", precedent.display());
+                 parameters.previous_path = current_dir;
+              }
+           }else {
+               if let Some(current) = &current_dir {
+                  println!("{}", current.display());
+               }
+               parameters.previous_path = current_dir;
+           }
+       }
+        path => {
+          let new_path = Path::new(path);
+            if let Err(_e) = set_current_dir(&new_path) {
+                eprintln!("⛔ 0-shell: No such file or directory {:?} 🫤", new_path);
+            }
+        }
+    }
+        // if &parameters.args[0] == "~" || &parameters.args[0] == "--" {
+        //     go_to_home();
+        //     return;
+        // }
+        // if parameters.args.len() == 1 && &parameters.args[0] == "-" {
+        //     if let Some(prev_dir) = parameters.previous_path.take() {
+        //         if let Err(e) = env::set_current_dir(&prev_dir) {
+        //             eprintln!("0-shell: cd: {}: {}", prev_dir.display(), e);
+        //             parameters.previous_path = current_dir.into();
+        //         } else {
+        //             println!("{}", prev_dir.display());
+        //             parameters.previous_path = current_dir;
+        //             println!("🚨 => {:?}", parameters.previous_path);
+        //         }
+        //     } else {
+        //         parameters.previous_path = env::current_dir().ok();
+        //     }
+        //     return;
+        // }
+        //     let path = Path::new(&parameters.args[0]);
+        //     if let Err(_e) = set_current_dir(&path) {
+        //         eprintln!("⛔ 0-shell: No such file or directory {:?}", path);
+        //     }
 }
 
 /*********🌟 go_to_home 🌟********/
