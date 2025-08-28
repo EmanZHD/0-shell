192d413495b2162b4356413bb88f67f13e219a13
hlamrani <hasnaelamrani3434@gmail.com>
2025-08-28 19:18:43 +0100

    echo start handling \

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -3,44 +3,41 @@ use crate::Params;
 /*********🌟 echo 🌟********/
 pub fn echo(parameters: &mut Params) {
     let mut output: Vec<String> = Vec::new();
-    // let mut compt_double = 0;
-    // let mut compt_single = 0;
     for word in &parameters.args {
         let new_word = word.trim();
+        let mut temp = String::new();
+        let mut count = 0;
         if new_word.starts_with("$") {
             if let Err(_e) = std::env::var(new_word.trim_matches('$')) {
-               continue;
-            }else {
-                println!(" ===> {}", std::env::var(new_word.trim_matches('$')).unwrap());
+                continue;
+            } else {
                 output.push(std::env::var(new_word.trim_matches('$')).unwrap());
-                println!("### output {:?}", output);
+                // print!("{}", std::env::var(new_word.trim_matches('$')).unwrap());
             }
-            
-        }else {
-        //    new_word = new_word.trim_matches(&['\'', '\"']);
-           output.push(new_word.to_string());
+        } else {
+            // new_word = new_word.trim_matches(&['\'', '\"']);
+            // output.push(new_word.to_string());
+            for c in word.chars() {
+                let mut charta= count % 4; // hadchy ghaaalt
+                println!("🌸 {}", charta);
+                if c == '\\' {
+                   count+=1;
+                }
+                if c != '\\' {
+                   temp.push(c);
+                   count = 0;
+                   charta = 0;
+                }
+                if charta > 0 {
+                   temp.push('\\');
+                }
+            }
+            output.push(temp);
+            temp = "".to_string();
         }
-    //     if new_word.starts_with("\"") {
-    //         compt_double+=1;
-    //     }
-    //     if new_word.starts_with("\'") {
-    //         compt_single+=1;
-    //     }
-    //     if new_word.ends_with("\"") {
-    //         compt_double+=1;
-    //     }
-    //     if new_word.ends_with("\'") {
-    //         compt_single+=1;
-    //     }
-        
-        
     }
-    //if compt_double%2 == 0 && compt_single%2 == 0 {
-        for i in output {
-           print!("{} ", i); 
-        }
-    // }else {
-    //     print!("⛔ Error!"); 
-    // }
+    for i in output {
+        print!("{} ", i);
+    }
     println!();
 }
