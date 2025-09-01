f182c2824cd28dcabfd7fec469bc4049f5226964
Hasnae 🍒 <125884813+Hasnaaaae@users.noreply.github.com>
2025-09-01 07:39:17 +0000

    echo $ => done for test

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -6,18 +6,59 @@ pub fn echo(parameters: &mut Params) {
     for word in &parameters.args {
         let new_word = word.trim();
         let mut temp = String::new();
-        if new_word.starts_with("$") {
-            if let Ok(var) = std::env::var(new_word.trim_matches('$')) {
-                output.push(var);
-            }
-            continue;
 
-        } 
+        if new_word == "$0" {
+            output.push("0-shell".to_string());
+        }
+
+        // if new_word.starts_with("$") {
+        //     if let Ok(var) = std::env::var(new_word.trim_matches('$')) {
+        //         output.push(var);
+        //     }
+        //     continue;
+        // } 
         
         let mut chars = new_word.chars().peekable();
         
         while let Some(c) = chars.next() {
-            if c == '\\' {
+            println!("==> {c}");
+            if c == '$' {
+                println!("### ==> {c}");
+               let mut dollar_count = 1;
+                while chars.peek() == Some(&'$') {
+                    chars.next();
+                    dollar_count += 1;
+                }
+                
+                if dollar_count == 1 {  // bach njam3 l'env variable
+                    let mut var_env = String::new();
+                    while let Some(&next_char) = chars.peek() {
+                        if next_char.is_alphanumeric() || next_char == '_' {
+                            var_env.push(chars.next().unwrap());
+                        } else {
+                            break;
+                        }
+                    }
+                    
+                    if var_env.is_empty() {
+                        temp.push('$');
+                    } else if let Ok(env_var) = std::env::var(&var_env) {
+                        temp.push_str(&env_var);
+                    }
+                } else {
+                    if dollar_count % 2 == 1 {
+                        for _ in 0..(dollar_count / 2) {
+                            temp.push_str(&std::process::id().to_string());
+                        }
+                        temp.push('$');
+                    } else {
+                        for _ in 0..(dollar_count / 2) {
+                            temp.push_str(&std::process::id().to_string());
+                        }
+                    }
+                }
+                println!("dollar {}", dollar_count);
+            } else if c == '\\' {
                 if let Some(next_char) = chars.next() {
             
                     match next_char {
