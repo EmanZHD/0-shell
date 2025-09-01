19df4fe4737b8f062e9ab479e56e0601ecb991cb
houda jeon <houdaafebts678@gmail.com>
2025-09-01 23:35:16 +0100

    fix mv cammand yaaaay 🥹​🥹​

--- src/commands/mv.rs
+++ src/commands/mv.rs
@@ -6,12 +6,12 @@ use crate::colors::{red, bold_gray, yellow, green, blue, bold_red, cyan};
 // 🥳 here for check args if it's valid or not 🥳
 pub fn mv(params: &mut Params) {
     if params.args.len() < 2 {
-        eprintln!("{}{}{}", bold_red("👀 ​mv: need at least 2 arguments\n"), green("👍 ​Usage: mv file1 file2\n"),  green("👍 ​or: mv files... folder"));        
+        eprintln!("{}{}{}", bold_red("👀 mv: need at least 2 arguments\n"), green("👍 Usage: mv file1 file2\n"),  green("👍 ​or: mv files... folder"));        
         return;
     }
 
-    let (sources, destination) = params.args.split_at(params.args.len() - 1);
-    let dest_path = Path::new(&destination[0]);
+    let (sources, des) = params.args.split_at(params.args.len() - 1);
+    let dest_path = Path::new(&des[0]);
     let is_dest_dir = dest_path.is_dir();
 
     if sources.len() > 1 && !is_dest_dir {
@@ -20,31 +20,35 @@ pub fn mv(params: &mut Params) {
     }
 
     for source in sources {
-        if let Err(e) = move_file(source, &destination[0], is_dest_dir) {
-            eprintln!( "{}", red(&format!("😸​ mv: cannot move '{}' ==> {}", yellow(source), e)));
-        }
+        if source == &des[0] && is_dest_dir {
+            eprintln!("{}", red(&format!("😸​ mv: cannot move '{}' to a subdirectory of itself, '{}/{}'", yellow(source), yellow(&des[0]), yellow(&des[0]))));
+        } else if source == &des[0] {
+            eprintln!("{}", red(&format!("😸​ mv: '{}' and '{}' are the same file", yellow(source), yellow(&des[0]))));
+        } else if let Err(e) = move_file(source, &des[0], is_dest_dir) {
+            eprintln!("{}", red(&format!("😸​ mv: cannot stat '{}': {}", yellow(source), e)));
+        } 
     }
 }
 
 // 💁‍♀️ here to move 💁‍♀️
-fn move_file(source: &str, destination: &str, dest_is_dir: bool) -> Result<(), Box<dyn std::error::Error>> {
+fn move_file(source: &str, des: &str, dest_is_dir: bool) -> Result<(), Box<dyn std::error::Error>> {
     let source_path = Path::new(source);
     
     if !source_path.exists() {
         return Err(bold_gray("No such file or directory").into());
     }
 
-     if dest_is_dir {
-        Path::new(destination).join(source_path.file_name().ok_or("Invalid filename")?)
+    let dest_path = if dest_is_dir {
+        Path::new(des).join(source_path.file_name().ok_or("Invalid filename")?)
     } else {
-        Path::new(destination).to_path_buf()
+        Path::new(des).to_path_buf()
     };
 
-    match fs::rename(source, destination) {
+    match fs::rename(source, &dest_path) {
         Ok(_) => {
             println!("{}", green(&format!("✓ Moved '{}' to '{}'", 
                 yellow(source), 
-                blue(destination)
+                blue(&dest_path.display().to_string()) 
             )));
         }
         Err(e) => {
