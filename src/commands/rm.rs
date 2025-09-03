4bae36f73006666deb7dae703191b72fd2eb7da1
fennadafy <fatimaezzahraennadafy@gmail.com>
2025-09-03 20:34:07 +0100

    update rm

--- src/commands/rm.rs
+++ src/commands/rm.rs
@@ -1,7 +1,12 @@
 use std::fs;
 // use crate::errors::CrateResult;
 use anyhow::anyhow;
+use std::fs::OpenOptions;
+use std::io;
 use std::path::Path;
+ use std::io::Write; 
+// ma3mdhach r ok
+//m3ndhach w non
 use crate::Params;
 pub fn rm(params: &mut Params) {
     match params.args.len() {
@@ -9,13 +14,29 @@ pub fn rm(params: &mut Params) {
         _ => {
             match params.args[0].as_str() {
                 "-r" => {
-                  for i in &params.args[1..] {
+                    for i in &params.args[1..] {
                         let path = Path::new(i);
                         if path.exists() {
                             if path.is_file() {
-                                match fs::remove_file(path) {
-                                    Ok(_) => {}
-                                    Err(_) => println!("err {}", path.display()),
+                                 if is_writable(path) {
+                                    match fs::remove_file(path) {
+                                        Ok(_) => {}
+                                        Err(_) => println!("err {}", path.display()),
+                                    }
+                                } else {
+                                    println!("rm: remove write-protected regular empty file '{}'? ", i);
+                                    io::stdout().flush().unwrap(); // printed befor waiting for input
+
+                                    let mut response = String::new();
+                                    if let Ok(_) = io::stdin().read_line(&mut response) {
+                                        let trimmed = response.trim().to_lowercase();
+                                        if trimmed == "y" || trimmed == "yes" {
+                                            match fs::remove_file(path) {
+                                                Ok(_) => {}
+                                                Err(_) => println!("err {}", path.display()),
+                                            }
+                                        }
+                                    }
                                 }
                             } else {
                                 match fs::remove_dir(path) {
@@ -29,16 +50,32 @@ pub fn rm(params: &mut Params) {
                                 path.display()
                             );
                         }
-                  }
+                    }
                 }
                 _ => {
                     for i in &params.args {
                         let path = Path::new(i);
                         if path.exists() {
                             if path.is_file() {
-                                match fs::remove_file(path) {
-                                    Ok(_) => {}
-                                    Err(_) => println!("err {}", path.display()),
+                                if is_writable(path) {
+                                    match fs::remove_file(path) {
+                                        Ok(_) => {}
+                                        Err(_) => println!("err {}", path.display()),
+                                    }
+                                } else {
+                                    println!("rm: remove write-protected regular empty file '{}'? ", i);
+                                    io::stdout().flush().unwrap(); // printed befor waiting for input
+
+                                    let mut response = String::new();
+                                    if let Ok(_) = io::stdin().read_line(&mut response) {
+                                        let trimmed = response.trim().to_lowercase();
+                                        if trimmed == "y" || trimmed == "yes" {
+                                            match fs::remove_file(path) {
+                                                Ok(_) => {}
+                                                Err(_) => println!("err {}", path.display()),
+                                            }
+                                        }
+                                    }
                                 }
                             } else {
                                 println!("rm: cannot remove '{}': Is a directory", path.display());
@@ -54,4 +91,7 @@ pub fn rm(params: &mut Params) {
             }
         }
     }
+}
+fn is_writable(path: &Path) -> bool {
+    OpenOptions::new().write(true).open(path).is_ok()
 }
